1f5e40d35884c844853596f9a0affabc16ab127b
Jonathan M <jmargolpeople@gmail.com>
2019-05-23 14:23:09 -0700

    Added a script for getting the necesary data

---
+++ revision/mashr/gatherData.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+testrun=true
+
+#example where I learned this from
+# ls -ld /storage/szfeupe/Runs/650GTEx_estr/Analysis_by_Tissue/* | grep '^d' | sed -e 's/\s\+/ /g' | cut -f 9 -d ' '
+
+#TODO: check this!
+tissuetypes='Adipose-Subcutaneous
+Adipose-Visceral(Omentum)
+Artery-Aorta
+Artery-Tibial
+Brain-Caudate(basalganglia)
+Brain-Cerebellum
+Cells-Transformedfibroblasts
+Esophagus-Mucosa
+Esophagus-Muscularis
+Heart-LeftVentricle
+Kidney-Cortex
+Liver
+Lung
+Muscle-Skeletal
+Nerve-Tibial
+Skin-NotSunExposed(Suprapubic)
+Skin-SunExposed(Lowerleg)
+Thyroid
+WholeBlood
+'
+
+if [ "$testrun" = true ]; then
+	#only use three tissues - two similar, one not, in testing
+	testtissues='Artery-Aorta Artery-Tibial Lung'
+	tissuetypes=$testtissues
+fi
+
+#Example command
+#cat /storage/szfeupe/Runs/650GTEx_estr/Analysis_by_Tissue/WholeBlood/Master.table | awk -F"\t" '{print $1 "\t" $2 "\t" $3 "\t" $7 "\t" $27}' | head
+tissuedir=/storage/szfeupe/Runs/650GTEx_estr/Analysis_by_Tissue
+for tissue in $tissuetypes ; do
+	command="cat ${tissuedir}/${tissue}/Master.table"' | awk -F"\t" '"'"'{print $1 "\t" $2 "\t" $3 "\t" $7 "\t" $27}'"'"
+
+	if [ "$testrun" = true ]; then
+		#only use chromosome 1 in testing
+		command="${command} | grep -P '(chr1\t)|(chrom)'"
+	fi
+	command="${command} > ./workdir/$tissue.table"
+	eval $command
+done
+
+
