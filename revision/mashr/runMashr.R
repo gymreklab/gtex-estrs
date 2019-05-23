ac694d6ee97baecf8b406d02e8d67eda271de879
Jonathan M <jmargolpeople@gmail.com>
2019-05-23 16:59:20 -0700

    I think we're loading the data in a format that mashr expects

---
+++ revision/mashr/runMashr.R
@@ -0,0 +1,33 @@
+#!/bin/R
+
+#------------------Load the data into two arrays----------------
+library('purrr')
+#get files to load
+files = list.files(path="workdir", pattern="*.table", full.names=TRUE)
+shortFileNames = purrr::map(files, function(name) basename(tools::file_path_sans_ext(name)))
+
+#load files individually
+#lung.table <- read.table("Lung.table", header=TRUE, colClasses=c('character', 'factor', 'integer', 'numeric', 'numeric'))
+dataFrameList = purrr::map(files, read.table, header=TRUE, colClasses=c('character', 'factor', 'integer', 'numeric', 'numeric'))
+
+#append the file name to the columns named beta and beta.se
+append = function (name) {
+	function (df, ext) {
+		names(df)[names(df) == name] = paste(name, ext, sep='_')
+		return(df)
+	}
+}
+
+dataFrameList = purrr::map2(dataFrameList, shortFileNames, append('beta'))
+dataFrameList = purrr::map2(dataFrameList, shortFileNames, append('beta.se'))
+
+allData = purrr::reduce(dataFrameList, function(df1, df2) base::merge(df1, df2, by=c('gene', 'chrom', 'str.start')))
+
+#move gene, str idetnifier info into rownames
+rownames(allData) = purrr::pmap(allData[c(1,2,3)], paste, sep='_')
+allData = allData[-(1:3)]
+betas = allData[!grepl('se', colnames(allData))]
+beta.ses = allData[grepl('se', colnames(allData))]
+
+#-------------------------Hand off to mashr------------------------
+library(mashr)
