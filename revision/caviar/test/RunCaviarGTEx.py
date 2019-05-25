5a9f4f810ba740913583fc3d9373897908bb3635
gymreklab <melissa.gymrek@gmail.com>
2019-05-24 21:48:09 -0700

    adding test caviar scripts

--- revision/caviar/test/RunCaviarGTEx.py
+++ revision/caviar/test/RunCaviarGTEx.py
@@ -7,6 +7,7 @@
 # Make STR and SNP genotypes indexed, maybe all in one file to keep sample order same?
 
 import argparse
+import os
 import pandas as pd
 import sys
 
@@ -14,8 +15,21 @@ def PROGRESS(msg, printit=True):
     if printit: # false for some messages when not in debug mode
         sys.stderr.write("%s\n"%msg.strip())
 
+def LoadReg(linreg, genes, prefix=""):
+    reg = pd.read_csv(linreg, sep="\t", usecols=["gene","chrom","str.start","beta","beta.se"])
+    reg = reg[reg["gene"].apply(lambda x: x in genes)]
+    reg["Z"] = reg.apply(lambda x: x["beta"]/x["beta.se"], 1)
+    reg["ID"] = reg.apply(lambda x: prefix+x["chrom"]+":"+x["str.start"], 1)
+    return reg
+
 # Write LDFILE, ZFILE
 def GenerateCAVIARFiles(gene, samples, strreg, snpreg, strgt, snpgt, tmpdir):
+    if not os.path.exists(os.path.join(tmpdir, gene)): os.mkdir(os.path.join(tmpdir, gene))
+    # 1. Get ZFILE
+    zfile = (os.path.join(tmpdir, gene, "ZFILE"))
+    strreg[strreg["gene"]==gene][["ID", "Z"]].to_csv(open(zfile, "w"), header=None, index=False)
+    snpreg[snpreg["gene"]==gene][["ID", "Z"]].to_csv(open(zfile, "a"), header=None, index=False)
+    # 2. Get LDFILE for only that set of variants
     pass # TODO
 
 # Run CAVIAR using LDFILE and ZFILE in tmp/
@@ -40,25 +54,27 @@ if __name__ == "__main__":
     parser.add_argument("--tmpdir", help="Use this directory for temporary files", type=str, default="/tmp")
     args = parser.parse_args()
 
-    # Load regression results
-    PROGRESS("\nLoad snps regression")
-    snpreg = pd.read_csv(args.linreg_snp, sep="\t", usecols=["gene","chrom","str.start","beta","beta.se"])
-    PROGRESS("\nLoad strs regression")
-    strreg = pd.read_csv(args.linreg_str, sep="\t", usecols=["gene","chrom","str.start","beta","beta.se"])
-
-    # Get list of samples to process
-    samples = [] # TODO get from args
-
     # Get list of genes to process
     if args.genes == "all": genes = set(strreg["gene"])
     else: genes = set(args.genes.split(","))
+
+    # Load regression results
+    if not args.precomputed:
+        PROGRESS("\nLoad snps regression")
+        snpreg = LoadReg(args.linreg_snp, genes, prefix="SNP_")
+        PROGRESS("\nLoad strs regression")
+        strreg = LoadReg(args.linreg_str, genes, prefix="STR_")
+
+        # Get list of samples to process
+        samples = [] # TODO get from args
 
     # For each gene:
     # 1. Get intermediate files
     # 2. Run CAVIAR
     # 3. Generate output
     for gene in genes:
+        PROGRESS("Processing gene %s"%gene)
         if not args.precomputed: # Store in args.tmpdir/gene/ LDFILE, ZFILE
-            GenerateCAVIARFiles(gene, samples, strreg, snpreg, args.strgt, args.snpgt, args.tmpdir)
+             GenerateCAVIARFiles(gene, samples, strreg, snpreg, args.strgt, args.snpgt, args.tmpdir)
         RunCAVIAR(gene, args.tmp)
         WriteOutput(gene, args.tmp, args.out)
