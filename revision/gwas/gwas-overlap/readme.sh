38d5d7bc8fbb905f2304b12f1d726e33a3bd1975
gymreklab <melissa.gymrek@gmail.com>
2019-07-26 16:26:40 -0700

    updating figures

--- revision/gwas/gwas-overlap/readme.sh
+++ revision/gwas/gwas-overlap/readme.sh
@@ -11,6 +11,9 @@
     --str /storage/szfeupe/Runs/650GTEx_estr/Filter_Merged_STRs_All_Samples_New.vcf.gz \
     --snp /storage/resources/datasets/gtex/59533/PhenoGenotypeFiles/RootStudyConsentSet_phs000424.GTEx.v7.p2.c1.GRU/GenotypeFiles/phg000830.v1.GTEx_WGS.genotype-calls-vcf.c1/GTEx_Analysis_2016-01-15_v7_WholeGenomeSeq_652Ind_GATK_HaplotypeCaller.vcf.gz
 
+# Get candidates for traits of interest
+./get_gwas_candidates_nhgri.sh
+
 # Print overlaps
 cat /storage/mgymrek/gtex-estrs/revision/figures/NCBI-072419_SuppDataset.tsv | cut -f 1,2 | sort | uniq | wc -l # 1381
 cat /storage/mgymrek/gtex-estrs/revision/figures/NCBI-072419_SuppDataset.tsv | grep -v nan | grep -v None | awk '($8>0.1)' | cut -f 1,2 | sort | uniq | wc -l # 847
